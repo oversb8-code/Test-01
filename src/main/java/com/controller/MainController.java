d51b9596f35c4e3b571c96904715cbbe97f9c16e
enji <oversb8@gmail.com>
2026-05-11 17:03:54 +0900

    푸시.

--- src/main/java/com/controller/MainController.java
+++ src/main/java/com/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.coffee.controller;
+package com.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/com/controller/pageController.java
+++ src/main/java/com/controller/pageController.java
@@ -1,10 +1,10 @@
-package com.coffee.controller;
+package com.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class PageController {
+public class pageController {
     @GetMapping("/first")
     public String first() { return "first";  }
 
